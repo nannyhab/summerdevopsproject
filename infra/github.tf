a3004af2bcd9ba5127827f2a1c2a5d9aa6592cb8
nannyhab <93690965+nannyhab@users.noreply.github.com>
2026-09-24 10:14:57 -0500

    applied the github actions config rules

---
+++ infra/github.tf
@@ -0,0 +1,41 @@
+variable "github_repo" {
+    type = string
+    default = "nannyhabers/summerdevopsproject"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+    url = "https://token.actions.githubusercontent.com"
+    client_id_list = ["sts.amazonaws.com"]
+}
+
+resource "aws_iam_role" "github_actions" {
+    name = "cluster-week-github-actions"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRoleWithWebIdentity"
+                Effect = "Allow"
+                Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+                Condition = {
+                    StringEquals = {
+                        "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+                    }
+                    StringLike = {
+                        "token.actions.githubusercontent.com:sub" = [
+                            "repo:${var.github_repo}:ref:refs/heads/main",
+                            "repo:${var.github_repo}:pull_request",
+                        ]
+                    }
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+    role       = aws_iam_role.github_actions.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_role_arn" {value = aws_iam_role.github_actions.arn}
